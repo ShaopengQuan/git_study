d0a1f6a4dff009c2c6500742bd1a6b163596d436
spquan <15650757870@163.com>
2022-08-02 00:54:07 +0800

    frist study thrift, save some codes
    Signed-off-by: spquan <15650757870@163.com>

---
+++ client.cc
@@ -0,0 +1,33 @@
+
+#include "./gen-cpp/Serv.h"
+#include <thrift/transport/TSocket.h>
+#include <thrift/transport/TBufferTransports.h>
+#include <thrift/protocol/TBinaryProtocol.h>
+#include <iostream>
+using namespace apache::thrift;
+using namespace apache::thrift::protocol;
+using namespace apache::thrift::transport;
+
+// using boost::shared_ptr;
+
+int main(int argc, char **argv)
+{
+    std::shared_ptr<TSocket> socket(new TSocket("127.0.0.1", 9090));
+    std::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
+    std::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+    transport->open();
+
+    //*****************添加部分******************
+    thrift::Test::Student s;
+    s.sno = 123;
+    s.sname = "xiaoshe";
+    s.ssex = 1;
+    s.sage = 30;
+    thrift::Test::ServClient client(protocol);
+    std::cout << "client send a data" << std::endl;
+    ;
+    client.put(s);
+    //*****************添加部分******************
+    transport->close();
+    return 0;
+}
